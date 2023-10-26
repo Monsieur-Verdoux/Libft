7fa0414613a7862ee3d4bce3f5557090f159b6da
Andrey Kovalevskiy <akovalev@student.hive.fi>
2023-10-26 14:05:45 +0300

    strlcat & strlcpy

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: akovalev <akovalev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 16:37:53 by akovalev          #+#    #+#             */
-/*   Updated: 2023/10/25 19:14:38 by akovalev         ###   ########.fr       */
+/*   Updated: 2023/10/26 14:03:36 by akovalev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,48 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	dstlen;
-	size_t	srclen;
 	size_t	i;
 
-	srclen = 0;
 	dstlen = 0;
 	i = 0;
-
-	while (src[srclen] != '\0')
-		srclen++;
-	while (dst[dstlen] != '\0' && dstlen < dstsize)
+	while (dstlen < dstsize && dst[dstlen] != '\0')
 		dstlen++;
-	while (i < dstsize - dstlen - 1 && src[i] != '\0')
+	if (dstlen == dstsize)
+		return (dstsize + ft_strlen(src));
+	while (src[i] != '\0' && dstlen + 1 < dstsize)
 	{
-		dst[dstlen + i] = src[i];
+		dst[dstlen] = src[i];
+		dstlen++;
 		i++;
 	}
-	return (dstlen + srclen);
+	dst[dstlen] = '\0';
+	while (src[i] != '\0')
+	{
+		dstlen++;
+		i++;
+	}
+	return (dstlen);
 }
+
+/*
+int	main(void)
+{
+	char	test[20] = "learning";
+	char	target[20] = "learning";
+	char	attach[20] = " is fun";
+	size_t	ft;
+	size_t	orginal;
+
+	printf("*** ft results ********\n");
+	ft = ft_strlcat(test, attach, 20);
+	printf("-> %s\n", test);
+	printf("*** orginal results ***\n");
+	orginal = strlcat(target, attach, 20);
+	printf("-> %s\n", target);
+	if (ft == orginal)
+		printf("The return values are the same");
+	else
+		printf("The return values are not the same");
+	return (0);
+}*/
 
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,30 +6,32 @@
 /*   By: akovalev <akovalev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 15:37:19 by akovalev          #+#    #+#             */
-/*   Updated: 2023/10/25 17:38:10 by akovalev         ###   ########.fr       */
+/*   Updated: 2023/10/26 14:04:37 by akovalev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+#include <stddef.h>
+
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	size_t	copylen;
 	size_t	srclen;
 
 	srclen = 0;
-	copylen = 0;
-	while (src[srclen] != '\0' && srclen < dstsize - 1)
-		srclen++;
-	while (copylen < srclen && copylen < dstsize - 1)
+
+	if (dstsize > 0)
 	{
-		dst[copylen] = src[copylen];
-		copylen++;
+		while (srclen < dstsize - 1 && src[srclen] != '\0')
+		{
+			dst[srclen] = src[srclen];
+			srclen++;
+		}
+		if (srclen < dstsize)
+			dst[srclen] = '\0';
 	}
-	dst[copylen] = '\0';
+
+	while (src[srclen] != '\0')
+		srclen++;
 	return (srclen);
 }
-
-/*int	main(void)
-{
-}*/
